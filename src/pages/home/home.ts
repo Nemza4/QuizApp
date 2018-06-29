38bcc2fb06401ba5486ae525760e6decb052d713
nemza4 <nemavholags@gmail.com>
2018-06-29 15:39:22 +0200

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,44 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  score = 0;
+  Lufuno1;Lufuno2;Lufuno3;
+  Nemza3;Nemza2;Nemza1;
+  Funi1;Funi2;Funi3;
+  Code1;Code2;Code3;
+  food1;food2;food3;
+  Dog;
+  Glorious;
   constructor(public navCtrl: NavController) {
 
-  }
 
+  }
+  submit(){
+
+
+    if(this.Lufuno1 == true ){
+    this.score = this.score +=1;
+    }
+
+
+     if(this.Nemza3 == true  ){
+      this.score = this.score +=1;
+    }
+    if(this.Nemza1 == true){
+      this.score = this.score +=1;
+
+    }
+    if(this.Code1 == true){
+      this.score = this.score +=1;
+    }
+
+    if(this.food1 == true){
+      this.score = this.score +=1;
+    }
+    if(this.Glorious == true){
+      this.score = this.score += 1;
+    }
+
+    console.log(this.score);
+  }
 }
